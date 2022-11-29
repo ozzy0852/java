a5ed8e34437c572ca44f3da1da0f7062d1bc7183
ozzy0852 <alexandrovartem62@gmail.com>
2022-11-29 19:47:20 +0100

    init

--- src/de/telrun/alexandrov/artjom/lesson4/task.java
+++ src/de/telrun/alexandrov/artjom/lesson4/task.java
@@ -4,7 +4,7 @@ public class task {
     public static void main(String[] args) {
         String str = "I study Basic Java!";
         System.out.println(str.charAt(18));
-        System.out.println(str.contains("фыдо"));
+        System.out.println(str.contains("java"));
         System.out.println(str.replace('a','o'));
         System.out.println(str.toUpperCase());
         System.out.println(str.toLowerCase());
