ed47e499c610ab1cb079b7b0c0124a9bb369688c
ozzy0852 <alexandrovartem62@gmail.com>
2022-12-04 17:07:22 +0100

    hw123

--- src/homework/homework4/task3.java
+++ src/homework/homework4/task3.java
@@ -1,6 +1,27 @@
 package homework.homework4;
 public class task3 {
-    public static void main(String[] args) {
+    class Cat {
+        void meow() {
+            // параметры отсутствуют
+        }
 
+        void meow(int count) {
+            // используется один параметр типа int
+        }
+
+        void meow(int count, int pause) {
+            // используются два параметра типа int
+        }
+
+        long meow(long time) {
+            // используется один параметр типа long
+            return time;
+        }
+
+        double meow(double time) {
+            // используется один параметр типа double
+            return time;
+        }
     }
+
 }
